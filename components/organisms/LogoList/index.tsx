d1af060ffc0bbaf3eef1e271f51d3b7d1bbbe388
Masayuki-JP <THU-Tokyo@outlook.com>
2023-08-22 18:54:06 +0800

    Shrink

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -8,22 +8,6 @@ const LogoList = () => {
       <section className="py-8 m-0 flex flex-row flex-wrap place-items-center justify-around gap-4 md:gap-0 border-y border-borderLight" data-aos='fade-left'>
         <div className="">
           <Image
-            src={'/images/github-logo.svg'}
-            width={120}
-            height="36"
-            alt="github logo"
-          />
-        </div>
-        <div className="">
-          <Image
-            src={'/images/forbes-logo.svg'}
-            width={120}
-            height="36"
-            alt="forbes logo"
-          />
-        </div>
-        <div className="">
-          <Image
             src={'/images/google-logo.svg'}
             width={120}
             height="36"
@@ -36,14 +20,6 @@ const LogoList = () => {
             width={120}
             height="36"
             alt="microsoft logo"
-          />
-        </div>
-        <div className="">
-          <Image
-            src={'/images/facebook-logo.svg'}
-            width={120}
-            height="36"
-            alt="facebook logo"
           />
         </div>
       </section>
