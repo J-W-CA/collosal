0ae23ec3b55ce5c0884b929061c06411c8df8db8
Masayuki-JP <THU-Tokyo@outlook.com>
2023-08-22 19:20:49 +0800

    Remove

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -6,14 +6,6 @@ const LogoList = () => {
   return (
     <>
       <section className="py-8 m-0 flex flex-row flex-wrap place-items-center justify-around gap-4 md:gap-0 border-y border-borderLight" data-aos='fade-left'>
-      <div className="">
-          <Image
-            src={'/images/Unity-ico.svg'}
-            width={120}
-            height="36"
-            alt="unity logo"
-          />
-        </div>
         <div className="">
           <Image
             src={'/images/google-logo.svg'}
