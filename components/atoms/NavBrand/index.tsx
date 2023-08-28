092c8e3a31dd881dd0a8f79272e562800acaa899
Masayuki-JP <THU-Tokyo@outlook.com>
2023-08-29 03:02:37 +0800

    auto

--- components/atoms/NavBrand/index.tsx
+++ components/atoms/NavBrand/index.tsx
@@ -14,7 +14,7 @@ const NavBrand = () => {
       >
         <Link href={'/'} passHref>
           <div className="w-fit h-fit">
-            <Image src={'/images/wbt.png'} width='135px' height={30} alt='company brand' loading='lazy' />
+            <Image src={'/images/wbt.png'} width='auto' height='30px' alt='company brand' loading='lazy' />
           </div>
         </Link>
       </div>
