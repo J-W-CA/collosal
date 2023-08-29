8d734ac9860bff4f2f388e96d66ef8e17153c02e
Masayuki-JP <THU-Tokyo@outlook.com>
2023-08-29 14:15:57 +0800

    WEBP

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,44 +9,45 @@ import { FiMail, FiPhoneCall } from 'react-icons/fi'
 
 const Contact = () => {
   return (
-    <>
-      <PageTemplate title='Contact - Collosal'>
-        <section className="grid grid-cols-1 place-items-center gap-10 lg:gap-5 lg:grid-cols-2">
-          <aside className="w-full sm:w-10/12 md:w-8/12 grid grid-cols-1 gap-12 sm:place-items-center lg:w-full lg:place-items-start" data-aos="fade-up-right">
-            <div className="sm:text-center lg:text-left">
-              <PageSentence
-                title="We love receiving messages from you, we are waiting for it."
-                badge="CONTACT"
-              />
-            </div>
-            <div className="space-y-6">
-              <IconListItem
-                label="Phone"
-                value="+1 (828)479-5148"
-                icon={<FiPhoneCall />}
-              />
-              <IconListItem
-                label="Email"
-                value="w@omnind.com"
-                icon={<FiMail />}
-              />
-            </div>
-          </aside>
-          <aside className="w-full sm:w-10/12 md:w-8/12 lg:w-full lg:flex lg:justify-end" data-aos="fade-down-left">
-            <div className="grid grid-cols-1 gap-7 p-6 md:p-9 bg-light rounded-md lg:w-10/12 ">
-              <div className="grid grid-cols-2 gap-4">
-                <InputGroup label="Name" />
-                <InputGroup label="Email" />
-              </div>
-              <InputGroup label="Subject" />
-              <TextAreaGroup label="Message" />
-              <Button value="Send Message" />
-            </div>
-          </aside>
-        </section>
-      </PageTemplate>
-    </>
-  )
+    <PageTemplate title='Contact - Collosal'>
+      <section className="grid grid-cols-1 place-items-center gap-10 lg:gap-5">
+        {/* 左侧名片内容 */}
+        <aside className="w-full sm:w-10/12 md:w-8/12 grid grid-cols-1 gap-12 sm:place-items-center lg:w-full lg:place-items-start" data-aos="fade-up-right">
+          <div className="sm:text-center lg:text-left">
+            <PageSentence
+              title="We love receiving messages from you, we are waiting for it."
+              badge="CONTACT"
+            />
+          </div>
+          <div className="space-y-6">
+            <IconListItem
+              label="Phone"
+              value="+1 (828)479-5148"
+              icon={<FiPhoneCall />}
+            />
+            <IconListItem
+              label="Email"
+              value="w@omnind.com"
+              icon={<FiMail />}
+            />
+          </div>
+        </aside>
+        {/* 右侧填充图片的卡片 */}
+        <div className="w-full sm:w-10/12 md:w-8/12 lg:w-full bg-light rounded-md p-6 md:p-9" data-aos="fade-down-left">
+          {/* 这里可以添加图片或其他内容 */}
+          {/* 示例： */}
+          <img src="/images/team-1.webp" alt="Filler Image" className="w-full h-auto rounded-md" />
+          {/* 或添加其他内容 */}
+        </div>
+      </section>
+    </PageTemplate>
+  );
 }
 
-export default Contact
+export default Contact;
+
+
+
+
+
+
