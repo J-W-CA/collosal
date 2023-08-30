ec34bae0e35948c4b504bae51ae6134160f464e8
Masayuki-JP <THU-Tokyo@outlook.com>
2023-08-31 01:05:58 +0800

    logo

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -8,17 +8,15 @@ const LogoList = () => {
       <section className="py-8 m-0 flex flex-row flex-wrap place-items-center justify-around gap-4 md:gap-0 border-y border-borderLight" data-aos='fade-left'>
         <div className="">
           <Image
-            src={'/images/Microsoft-Logo.png'}
-            width={120}
-            height="36"
+            src={'/images/Microsoft-Solutions-Partner.png'}
+            width={200}
             alt="google logo"
           />
         </div>
         <div className="">
           <Image
             src={'/images/Microsoft Cloud Solution Provider.svg'}
-            width={120}
-            height="36"
+            width={200}
             alt="microsoft logo"
           />
         </div>
