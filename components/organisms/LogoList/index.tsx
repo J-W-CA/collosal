742156316fca7e950d52657ab0482099d5dca82a
Masayuki-JP <THU-Tokyo@outlook.com>
2023-11-18 00:25:23 +0800

    roll back

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -15,17 +15,6 @@ const LogoList = () => {
           />
         </div>
       </section>
-
-      <section className="py-8 m-0 flex flex-row flex-wrap place-items-center justify-around gap-4 md:gap-0 border-y border-borderLight" data-aos='fade-left'>
-        <div className="">
-          <Image
-            src={'/images/salesforce.png'}
-            width={1200}
-            height="360"
-            alt="Salesforce partner logo"
-          />
-        </div>
-      </section>
     </>
 
 
